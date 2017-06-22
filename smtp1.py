5fada126f51e5e81e0279dd7b6f87cf8ac3bd6df
ad <ad@itmp.top>
2017-06-22 10:18:50 +0800

    smtp

--- smtp1.py
+++ smtp1.py
@@ -1,28 +1,28 @@
-
+#-*-coding:UTF-8 -*-
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
-#  这是定义几个需要使用的变量
 smtp_host='smtp.mxhichina.com'
 smtp_port=25
 smtp_user='hello@ifnot.cc'
 smtp_pass='jie00.123'
 
-sender=smtp_user # 邮件发送者
-receivers = ['mm@ifnot.cc', '403324737@qq.com']# 邮件接受者们
+sender=smtp_user
+receivers=['hello@ifnot.cc','403324737@qq.com']
 
-message = MIMEText('测试邮件！！！')
-message['From'] = Header('你好', 'utf-8')
-message['To'] = Header('这是测试', 'utf-8')
+message = MIMEText('喵喵,么么哒~')
+message['From'] = Header('小甜甜','utf-8')
+message['To'] = Header('小笨笨','utf-8')
 
-subject = 'python smtp 发送邮件测试'
-message['Subject'] = Header(subject, 'utf-8')
+subject = 'python smtp send e-mails'
+message['Subject']=Header(subject,'utf-8')
 
 try:
     smtp_server = smtplib.SMTP()
     smtp_server.connect(smtp_host, smtp_port)
-    smtp_server.login(smtp_user, smtp_pass)
-    smtp_server.sendmail(sender, receivers, message.as_string())
+    smtp_server.login(smtp_user,smtp_pass)
+    smtp_server.sendmail(sender,receivers,message.as_string())
+
 except smtplib.SMTPException:
-    print("Error: 无法发送邮件")
+    print("Error: can't send e-mails")
--- test.py
+++ test.py
@@ -1,7 +1,27 @@
+import unittest
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import page
 
-driver = webdriver.Remote(
-    command_executor='http://127.0.0.1:4444/wd/hub',
-    desired_capabilities=DesiredCapabilities.CHROME
-)
+class PythonOrgSearch(unittest.TestCase):
+    """A sample test class to show how page object works"""
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get('http://www.python.org')
+
+    def test_search_in_python_org(self):
+        main_page = page.MainPage(self.driver)
+        assert main_page.is_title_matches(), "python.org title doesn't match."
+        main_page.search_text_element = "pycon"
+        main_page.click_go_button()
+        search_results_page = page.SearchResultsPage(self.driver)
+        assert search_results_page.is_result_found, "No results found."
+
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
